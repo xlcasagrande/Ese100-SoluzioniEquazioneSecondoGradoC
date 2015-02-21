0f9a791d707572a111a643704e9539e2af937f1c
Francesco Trotta <frtrotta@users.noreply.github.com>
2015-02-21 12:34:25 +0100

    Da completare. Vedi README.md per istruzioni.

--- main.c
+++ main.c
@@ -6,24 +6,7 @@
 
 int main(int argc, char** argv) {
     
-    float a, b, c;
-    complexNumber x1, x2;
-    
-    puts("Benvenuto nel più innvoativo software di calcolo soluzioni per equazioni");
-    puts("di secondo grado in forma canonica: a*x^2 + b*x + c = 0\n");
-    
-    printf("Inserisci il coefficiente a: ");
-    scanf("%f", &a);
-    printf("Inserisci il coefficiente b: ");
-    scanf("%f", &b);
-    printf("Inserisci il coefficiente c: ");
-    scanf("%f", &c);
-    
-    x1 = computeX1(a, b, c);
-    x2 = computeX2(a, b, c);
-    
-    printf("\nx1: %s\nx2: %s\n", formatComplexNumber(x1), formatComplexNumber(x2));
-    
+
     return (EXIT_SUCCESS);
 }
 
--- quadraticEquationSolver.h
+++ quadraticEquationSolver.h
@@ -3,8 +3,6 @@
 
 #include "complexNumbers.h"
 
-double computeDeterminant(float a, float b, float c);
-complexNumber computeX1(float a, float b, float c);
-complexNumber computeX2(float a, float b, float c);
+
 
 #endif
